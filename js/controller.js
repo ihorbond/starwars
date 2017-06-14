64f6f84088375bfadcb92836422b74cfd6e0b4df
ihorbond <ihorbond@gmail.com>
2017-06-14 16:35:22 -0400

    redone logic

--- js/controller.js
+++ js/controller.js
@@ -1,144 +1,160 @@
 // $(document).ready(function (){
-  console.log('controller online');
-
+// console.log('controller online');
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
-var baseR = 100;
-var score = 0;
-var baseStatus = 'unshielded';
-var round = 1;
-var playerSide = 'sith';
 var rebellionShip = new Image();
-rebellionShip.src = './img/alliance_ship_resized.png';
+rebellionShip.src = './img/rebellion_ship.png';
 var sithShip = new Image();
-sithShip.src = './img/sith_ship_resized.png';
-var planet = new Image();
+sithShip.src = './img/empire_ship.png';
 var background = new Image();
-var ships = [new ShipBuilder(rebellionShip, 0), new ShipBuilder(sithShip, 1), new ShipBuilder(rebellionShip, 2), new ShipBuilder(sithShip, 3)];
-var time = 0;
 background.src = '../starwars/img/background.png';
+var explosion2 = new Image();
+explosion2.src = './img/explosion2.png';
+var planet = new Image();
+var ships = new Array(200);
 var mainTheme = new Audio('./audio/maintheme.mp3');
 var yoda = new Audio('./audio/yoda.mp3');
 var veider = new Audio('./audio/veider.mp3');
+var playerSide;
+var baseR = 100;
+var score = 0;
+var time = 0;
+var nextMoveX;
+var nextMoveY;
+var shieldHealth = 2;
+var showCollision = false;
+var showExplosion = false;
+var currentShip; //global variable for current ship object
 mainTheme.volume = 0.3;
 mainTheme.loop = true;
-document.addEventListener("click", mouseHandler);
+document.addEventListener("mousedown", mouseHandler);
 document.addEventListener("touchstart", touchHandlerStart);
-document.addEventListener("touchend", touchHandlerEnd);
 document.getElementById('score').innerHTML = score;
 
-
-
-function ShipBuilder(shipType, index) {
+function ShipBuilder(shipType) {
   this.shipImage = shipType;
-  this.index = index;
   this.radius = 50;
-  this.x = 0;
-  this.y = Math.floor((Math.random() * 800));
-  this.dx = Math.floor((Math.random() * 5) + 1);
-  this.dy = Math.floor((Math.random() * 5) + 1);
+  this.x = 1;
+  this.y = Math.floor((Math.random() * 799) + 1);
+  this.dx = Math.floor((Math.random() * 8) + 2);
+  this.dy = Math.floor((Math.random() * 8) + 2);
   this.edgeDetection = function() {
-   if (this.y + this.dy + this.radius > canvas.height || this.y + this.dy < 0) {
-      // console.log('bottom or top edge detected');
-     this.dy = -this.dy;
-  }
-    else if (this.x + this.dx + this.radius > canvas.width || this.x + this.dx < 0){
-      // console.log('left or right edge detected');
-     this.dx = -this.dx;
-   }
- };
- this.checkCollision = function () {
-   var d = Math.sqrt(Math.pow((this.x - canvas.width / 2), 2) + Math.pow((this.y - canvas.height / 2), 2));
-   if (d < baseR) {
-     console.log('collision detected');
-     ships.splice(index,1);
-     score += 200;
-     document.getElementById('score').innerHTML = score;
-   }
- };
+    if (this.y + this.dy + this.radius + this.radius / 2 > canvas.height || this.y + this.dy < 0) {
+      this.dy = -this.dy;
+    } else if (this.x + this.dx + this.radius + this.radius / 2 > canvas.width || this.x + this.dx < 0) {
+      this.dx = -this.dx;
+    }
+  };
+  this.checkCollision = function(index, oneShip) {
+    // d = distance from ship's center to the center of canvas aka base
+    var d = Math.sqrt(Math.pow((this.x + this.radius - canvas.width / 2), 2) + Math.pow((this.y + this.radius - canvas.height / 2), 2));
+    if (d < baseR + this.radius/2 ) {
+      //shield health will depend on the total amount of ships on canvas
+      showCollision = true;
+      score -= 100;
+      shieldHealth = 2 - 2 / ships.length;
+      ships.splice(index, 1);
+      document.getElementById('score').innerHTML = score;
+    }
+  };
 }
 
-function baseSelector() {
-  if (playerSide === 'rebellion') {
-    planet.src = '../starwars/img/earth.png';
-  } else {
-    planet.src = '../starwars/img/death_star.png';
-  }
-  return planet;
+function fillShipsArrayAndBaseSetup() {
+  var i = 0;
+  var intervalId = setInterval(function () {
+    if ( i < ships.length) {
+      if (playerSide === 'rebellion') {
+        planet.src = '../starwars/img/death_star.png';
+        ships[i] = new ShipBuilder(rebellionShip);
+      } else {
+        planet.src = '../starwars/img/rebellion_base.png';
+        ships[i] = new ShipBuilder(sithShip);
+      }
+      i++;
+    }
+    else {
+      clearInterval(intervalId);
+    }
+  }, 1000);
 }
 
 function mouseHandler(e) {
-  var relativeX = e.clientX;
-  var relativeY = e.clientY;
-  if (relativeX > 450 - baseR && relativeY > 300 - baseR && baseStatus === 'unshielded' || relativeX > 450 + baseR && relativeY > 300 + baseR && baseStatus === 'unshielded') {
-    console.log('base shielded');
-    baseStatus = 'shielded';
-  } else {
-    console.log('base unshielded');
-    baseStatus = 'unshielded';
-  }
+destroyShip(e);
 }
 
 function touchHandlerStart(e) {
-  var relativeX = e.clientX;
-  var relativeY = e.clientY;
-  if (relativeX > 450 - baseR && relativeY > 300 - baseR && baseStatus === 'unshielded' || relativeX > 450 + baseR && relativeY > 300 + baseR && baseStatus === 'unshielded') {
-    console.log('base shielded');
-    baseStatus = 'shielded';
-  }
+  destroyShip(e);
 }
 
-function touchHandlerEnd(e) {
-  var relativeX = e.clientX;
-  var relativeY = e.clientY;
-  if (relativeX > 450 - baseR && relativeY > 300 - baseR && baseStatus === 'unshielded' || relativeX > 450 + baseR && relativeY > 300 + baseR && baseStatus === 'unshielded') {
-    console.log('base unshielded');
-    baseStatus = 'unshielded';
+function destroyShip(e) {
+  var rect = e.target.getBoundingClientRect(); //tie mouse coordinates to canvas
+  var x = e.pageX - rect.left;
+  var y = e.pageY - rect.top;
+  ships.forEach(function (ship, index) {
+  var d = Math.sqrt(Math.pow((ship.x + ship.radius - x), 2) + Math.pow((ship.y + ship.radius - y), 2));
+  if (d < ship.radius) {
+    currentShip = ship;
+    showExplosion = true;
+    score += 300;
+    document.getElementById('score').innerHTML = score;
+    ships.splice(index, 1);
   }
+  });
 }
 
-
-
-  // if (d < baseR && baseStatus === 'sheilded' && playerSide === 'rebellion' && ship === 'rebellion') {
-  //   console.log("lost rebellion ship!");
-  //   score -= 200;
-  //   clearInterval(intervalID);
-  // } else if (d < baseR && baseStatus === 'unshielded' && playerSide === 'rebellion' && ship === 'rebellion') {
-  //   console.log("rebellion ship made it home!");
-  //   score += 200;
-  //   clearInterval(intervalID);
-  // } else if (d < baseR && baseStatus === 'shieded' && playerSide === 'rebellion' && ship === 'sith') {
-  //   console.log("destroyed sith ship!");
-  //   score += 200;
-  //   clearInterval(intervalID);
-  // } else if (d < baseR && baseStatus === 'unshilded' && playerSide === 'rebellion' && ship === 'sith') {
-  //   console.log("sith ship got thru!");
-  //   score -= 200;
-  //   clearInterval(intervalID);
-  // }
-  //
-
+var animation = requestAnimationFrame(draw);
 function draw(timer) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background, 0, 0);
-  ctx.drawImage(baseSelector(), 500, 300);
-  ships.forEach(function (oneShip, index) {
+  ctx.drawImage(planet, 500, 300);
+  ctx.beginPath();
+  //painting base shield
+  ctx.arc(600, 400, baseR+3, 0, shieldHealth * Math.PI);
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  //painting ships taken from ships array
+  ships.forEach(function(oneShip, index) {
     ctx.drawImage(oneShip.shipImage, oneShip.x, oneShip.y);
+    nextMoveX = oneShip.dx + oneShip.x;
+    nextMoveY = oneShip.dy + oneShip.y;
     oneShip.x += oneShip.dx;
     oneShip.y += oneShip.dy;
     oneShip.edgeDetection();
-    oneShip.checkCollision();
+    oneShip.checkCollision(index, oneShip);
+    //painting circles around ships
+    ctx.beginPath();
+    ctx.arc(nextMoveX + oneShip.radius / 2, nextMoveY + oneShip.radius / 2, oneShip.radius, 0, 2 * Math.PI);
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    setTimeout(function() {
+    }, 2000);
   });
-  var countdown = Math.floor(timer/1000);
+  if (showCollision) {
+    setTimeout(function() {
+      showCollision = false;
+    }, 100);
+    ctx.beginPath();
+    ctx.arc(600, 400, baseR + 1, 0, 2 * Math.PI);
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+  }
+  if (showExplosion) {
+    setTimeout(function() {
+      showExplosion = false;
+    }, 100);
+    ctx.drawImage(explosion2, currentShip.x, currentShip.y);
+  }
+  //implementing countdown timer
+  var countdown = -(Math.floor(timer / 1000) - 60);
   document.getElementById('timer').innerHTML = countdown;
-if (timer > 600000) {
-console.log('game over');
+  if (countdown === 0 || ships.length === 0 || shieldHealth < 0) {
+    console.log('game over');
+    gameOver();
+    cancelAnimationFrame(animation);
+  } else {
+    requestAnimationFrame(draw);
+  }
 }
-else {
-requestAnimationFrame(draw);
-}
-// console.log(timestamp);
-}
-draw();
-// document.getElementById('')
-// });
--- js/view.js
+++ js/view.js
@@ -1,79 +1,99 @@
 // $(document).ready(function() {
-console.log('view online');
-  window.onload = function () {
-    mainTheme.play();
-    $('#myCanvas').hide();
-    $('.header').hide();
-    $('.jumbotron').hide();
-    $('.instructions').hide();
-    $('.about').hide();
-  };
-  var isGameOn = false;
-  var musicVolumeOn = false;
+// console.log('view online');
+window.onload = function() {
+  mainTheme.play();
+  $('#myCanvas').hide();
+  $('.header').hide();
+  $('.jumbotron').hide();
+  $('.instructions').hide();
+  $('.about').hide();
+  $('.game-over').hide();
+};
+var isGameOn = false;
+var musicVolumeOn = false;
 
-  function launch() {
-    $('#disclaimer').hide();
-    $('.jumbotron').hide();
-    $('.instructions').hide();
-    $('about').hide();
-    $('.header').show();
-    $('#myCanvas').show();
-    requestAnimationFrame(draw);
+function gameOver() {
+  $('#myCanvas').show();
+  $('.jumbotron').hide();
+  $('.instructions').hide();
+  $('.about').hide();
+  $('.game-over').hide();
+  $('#disclaimer').hide();
+  $('.game-over').hide();
+  $('#final-score').html(score);
+  $('#highest-score').html(ships.length * 200);
+  if (playerSide === 'rebellion' && shieldHealth > 0 || playerSide === 'sith' && shieldHealth < 0) {
+    console.log('Empire won this battle!');
   }
-  document.getElementById('new-game').onclick = function() {
-    if (!isGameOn) {
-      isGameOn = true;
-      $('#disclaimer').hide();
-      $('.jumbotron').show();
-      $('.instructions').hide();
-      $('.header').hide();
-      $('#myCanvas').hide();
-      $('.about').hide();
-    } else {
-      location.reload();
-    }
-  };
-  document.getElementById('sith-selected').onclick = function() {
-    playerSide = 'sith';
-    launch();
-  };
-  document.getElementById('sith-selected').onmouseover = function() {
-      veider.play();
-  };
-  document.getElementById('rebellion-selected').onclick = function() {
-    playerSide = 'rebellion';
-    launch();
-  };
-  document.getElementById('rebellion-selected').onmouseover = function() {
-      yoda.play();
-  };
-  document.getElementById('mute').onclick = function () {
-    if (!musicVolumeOn) {
-      musicVolumeOn = true;
-      mainTheme.volume = 0;
-    }
-    else {
-      musicVolumeOn = false;
+  else {
+    console.log('Alliance won this battle!');
+  }
+}
+
+function launch() {
+  $('.header').show();
+  $('#myCanvas').show();
+  $('#disclaimer').hide();
+  $('.jumbotron').hide();
+  $('.instructions').hide();
+  $('about').hide();
+  fillShipsArrayAndBaseSetup();
+  // var animation = requestAnimationFrame(draw);
+}
+document.getElementById('new-game').onclick = function() {
+  if (!isGameOn) {
+    isGameOn = true;
+    $('.jumbotron').show();
+    $('.game-over').hide();
+    $('#disclaimer').hide();
+    $('.instructions').hide();
+    $('.header').hide();
+    $('#myCanvas').hide();
+    $('.about').hide();
+
+  } else {
+    location.reload();
+    score = 0;
+  }
+};
+document.getElementById('sith-selected').onclick = function() {
+  playerSide = 'sith';
+  launch();
+};
+document.getElementById('sith-selected').onmouseover = function() {
+  veider.play();
+};
+document.getElementById('rebellion-selected').onclick = function() {
+  playerSide = 'rebellion';
+  launch();
+};
+document.getElementById('rebellion-selected').onmouseover = function() {
+  yoda.play();
+};
+document.getElementById('mute').onclick = function() {
+  if (!musicVolumeOn) {
+    musicVolumeOn = true;
+    mainTheme.volume = 0;
+  } else {
+    musicVolumeOn = false;
     mainTheme.volume = 0.3;
   }
-    };
-    document.getElementById('instructions').onclick = function() {
-      $('#disclaimer').hide();
-      $('.jumbotron').hide();
-      $('.instructions').show();
-      $('.header').hide();
-      $('#myCanvas').hide();
-      $('.about').hide();
-    };
+};
+document.getElementById('instructions').onclick = function() {
+  $('.instructions').show();
+  $('#disclaimer').hide();
+  $('.jumbotron').hide();
+  $('.header').hide();
+  $('#myCanvas').hide();
+  $('.about').hide();
+};
 document.getElementById('about').onclick = function() {
+  $('.about').show();
   $('#disclaimer').hide();
   $('.jumbotron').hide();
   $('.instructions').hide();
   $('.header').hide();
   $('#myCanvas').hide();
-  $('.about').show();
 };
-
-
 
 // });
